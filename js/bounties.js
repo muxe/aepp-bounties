d8d233c2ad347a3df3fd46426b67e6a45430cf30
muxe <max@apeunit.com>
2018-02-22 16:15:19 +0100

    yay

---
+++ js/bounties.js
@@ -0,0 +1,30 @@
+const queryProjects = async (options) => {
+  const combine = options.combine || false
+  const labels = options.labels || ['bounty']
+  const state = options.state || 'all'
+  const projects = options.projects || []
+
+  const urls = projects.map(project => {
+    return `https://api.github.com/repos/${project}/issues?labels=${labels.join(',')}&state=${state}`
+  })
+
+  const tasks = urls.map(url => {
+    return axios.get(url)
+  })
+
+  try {
+    const results = await Promise.all(tasks)
+    const resultDatas = results.map(result => {
+      return result.data
+    })
+    if (combine) {
+      console.log(JSON.stringify([].concat(...resultDatas), null, 2))
+      return [].concat(...resultDatas)
+    } else {
+      return resultDatas
+    }
+  } catch (e) {
+    console.log(e)
+    return []
+  }
+}
---
+++ js/header.js
@@ -0,0 +1,15 @@
+Vue.component('ae-header', {
+  template: `
+  <header>
+      <div class="container">
+          <a class="logo" href="/">
+              <img src="/img/logo.svg" alt="Aeternity">
+          </a>
+
+          <a href="http://aepps.com" class="btn">æpps.com</a>
+
+          <button id="open-menu" class="btn"></button>
+      </div>
+  </header>
+  `
+})
